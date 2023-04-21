a6e92918aa2cb3ab7f230e0e8b6701d247c941b0
essa1964 <essa1964@colorado.edu>
2023-04-20 20:31:36 -0600

    wa13 finished
    My god why did I do this to myself

--- wa/wa13/wa13.js
+++ wa/wa13/wa13.js
@@ -5,12 +5,28 @@ function check() {
 }
 
 function submit() {
-    alert(output.textContent);
+    for (let i=0; i<numbers.length; i++) {
+        if (words === numbers[i]) {
+            alert("Volume:  " + output.textContent);
+            console.log("This works");
+        }
+    }
+}
+
+function randomPos() {
+    const element = document.getElementById("letter-button");
+    for (const child of element.children) {
+        //console.log(child);
+        child.style.top = Math.random()*3000 + "px";
+        child.style.left = Math.random()*4000 + "px";
+    }
 }
 
 function reset() {
     outputInt = 0;
     output.textContent = outputInt;
+    words = "";
+    console.log(words);
 }
 
 function minus() {
@@ -26,6 +42,15 @@ function plus() {
     output.textContent = outputInt;
     }
 }
+
+
+
+function letters() {
+    const letter = document.getElementById('letter-button');
+    console.log(letter);
+}
+
+
 
 function random() {
     outputInt = randomNumber(0, 100);
@@ -43,32 +68,56 @@ const output = document.querySelector('.output');
 let outputInt = parseInt(output.textContent);
 console.log(outputInt);
 
-const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
-const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
+var words = "";
+let numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty', 'twenty-one', 'twenty-two', 'twenty-three', 'twenty-four', 'twenty-five', 'twenty-six', 'twenty-seven', 'twenty-eight', 'twenty-nine', 'thirty', 'thirty-one', 'thirty-two', 'thirty-three', 'thirty-four', 'thirty-five', 'thirty-six', 'thirty-seven', 'thirty-eight', 'thirty-nine', 'forty', 'forty-one', 'forty-two', 'forty-three', 'forty-four', 'forty-five', 'forty-six', 'forty-seven', 'forty-eight', 'forty-nine', 'fifty', 'fifty-one', 'fifty-two', 'fifty-three', 'fifty-four', 'fifty-five', 'fifty-six', 'fifty-seven', 'fifty-eight', 'fifty-nine', 'sixty', 'sixty-one', 'sixty-two', 'sixty-three', 'sixty-four', 'sixty-five', 'sixty-six', 'sixty-seven', 'sixty-eight', 'sixty-nine', 'seventy', 'seventy-one', 'seventy-two', 'seventy-three', 'seventy-four', 'seventy-five', 'seventy-six', 'seventy-seven', 'seventy-eight', 'seventy-nine', 'eighty', 'eighty-one', 'eighty-two', 'eighty-three', 'eighty-four', 'eighty-five', 'eighty-six', 'eighty-seven', 'eighty-eight', 'eighty-nine', 'ninety', 'ninety-one', 'ninety-two', 'ninety-three', 'ninety-four', 'ninety-five', 'ninety-six', 'ninety-seven', 'ninety-eight', 'ninety-nine', 'one-hundred'];
+
+
+// const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
+// const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
 const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
-const randomButton = document.querySelector('.random-button').addEventListener('click', random);
-const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
+// const randomButton = document.querySelector('.random-button').addEventListener('click', random);
+const submitButton = document.querySelector('.submit-button').addEventListener('click', submit); 
+const letterButton = document.querySelector('#letter-button').addEventListener('click', (event) => {
+    const el = event.target.value;
+    words = words + el;
+    output.innerHTML = words;
+    // all of this is just to see if this works
+    // console.log(words);
+    // console.log(el);
+    // for (let i=0; i<numbers.length; i++) {
+    //     if (words === numbers[i]) {
+    //         console.log("This works")
+    //     }
+    // }
+    
+    const isButton = event.target.nodeName === 'BUTTON';
+    if (!isButton) {
+        return;
+    }
+} 
+);
+randomPos();
 
 
-/* const button = document.querySelector('.button');
-const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
+// const button = document.querySelector('.button');
+// const output = document.querySelector('.output');
+// let phone_content = document.querySelector('.phone');
 
-button.addEventListener('click', updateOutput);
+// button.addEventListener('click', updateOutput);
 
-function updateOutput() {
-    output.textContent = phone_content.value;
-    alert(phone_content.value);
-}
-*/
+// function updateOutput() {
+//     output.textContent = phone_content.value;
+//     alert(phone_content.value);
+// }
 
 
-var slider = document.getElementById("myRange");
-var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
-var sliderOutput = document.querySelector(".slider-output");
+
+// var slider = document.getElementById("myRange");
+// var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
+// var sliderOutput = document.querySelector(".slider-output");
 
 
 // Update the current slider value (each time you drag the slider handle)
-function update() {
-  sliderOutput.textContent = slider.value;
-}
+// function update() {
+//   sliderOutput.textContent = slider.value;
+// }
